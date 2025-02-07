f0edbee78fe4fcc931e6b347a67cb2e2bcf73bd8
Rhonda Bacher <rhondabacher@users.noreply.github.com>
2025-02-07 12:14:49 -0500

    Update app1_ui.R
    Not ready for PR or merge yet, in progress.

--- R/app1_ui.R
+++ R/app1_ui.R
@@ -17,8 +17,8 @@ databuilderModuleUI <- function(id) {
           choices = list("GLM", "GLMM", "GEE")
         ),
         numericInput(ns("n_potential_basis_fns"),
-          label = "No. of basis functions (optional, defaults to 5, recommended between 3-7)",
-          value = 5,
+          label = "No. of basis functions (optional, defaults to 3, recommended between 2-5)",
+          value = 3,
           min = 1
         ),
         numericInput(ns("highly_variable_genes"),
@@ -38,12 +38,46 @@ databuilderModuleUI <- function(id) {
           label = "Name of cell offset column in metadata (optional)",
           value = ""
         ),
+        textInput(ns("subject_id"),
+          label = "Name of subject id column in metadata (required for GEE or GLMM)",
+          value = ""
+        ),
         actionButton(ns("generate"), "Generate"),
         downloadButton(ns("download_file"), "Download File", style = "display: none;")
       ),
       mainPanel(
         h4("Instructions:"),
-        p("Fill in the required fields and upload the necessary files to generate the executable file for scLANE analysis."),
+        p("Fill in the required fields to generate the executable file for scLANE analysis.
+          
+      <br>
+Input:
+
+xxAdd examples of model objects screenshot.XX
+
+Model type:
+
+Number of basis functions: 
+
+Number of highly variable genes: Select how many highly variable genes to test using scLANE.
+If HVG's have already been identified and saved in the Seurat object, they will be pulled directly. 
+Otherwise, HVG calculation will be performed as XX. Alternatively, you may submit a vector of genes for
+testing by naming a list object as HVG in your custom RDS upload.
+
+Random seed: Enter a value for reproducibility.
+
+Pseudotime column: This should be provided in the Seurat object metadata or as a vector in a 
+named list object (enter the object name in this case).
+
+<p> Cell offset column: If provided, this should be in the Seurat object metadata or as a vector in a 
+named list object (enter the object name in this case). If not provided, offsets will be created as counts-per-10k. In general, 
+the offset represents the scaling factor used to account for sequencing depth factor differences 
+across cells. </p>
+
+Subject ID column: If GEE or GLMM mode are selected, then a Seurat metadata column or named list
+object containing the subject IDs is required.
+  
+
+"),
         htmlOutput(ns("validation_message"), style = "color: red; font-weight: bold;")
       )
     )
